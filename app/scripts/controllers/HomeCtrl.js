1626df228a4ba65b16e8a691db0a6826acf001b7
JacobYun <jacob.yun429@gmail.com>
2017-07-01 18:20:36 -0700

    completed checkpoint-2

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,10 @@
 (function() {
-    function HomeCtrl() {
-
+    function HomeCtrl(Room) {
+      //assign Room array retrieved by all method to $scope variable
+        this.rooms = Room.all;
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', [HomeCtrl]);
+        .controller('HomeCtrl', ['Room', HomeCtrl]);
 })();
